cfef0960520df4af449677ab4c7c88bc9b881d4d
danici <danici@email.com>
2023-12-14 07:05:26 +0100

    Installation and intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  // const { data, error, isLoading } = useQuery({key: ['test'], fn:() => {
+  //   return axios('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+  //   }
+  // })
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>{
+      return axios('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+      //return response
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error ? <p>Error on display</p> : null}
+      {isLoading ? <p>Loading....</p> : null}
+      {data?.data?.bitcoin?.usd ? data.data.bitcoin.usd : null}
     </div>
   );
 }
