e8a93062066ad9f8e46c53a45f3a7b79b764bf38
danici <danici@email.com>
2023-12-18 07:47:13 +0100

    Caching dada 1

--- src/App.js
+++ src/App.js
@@ -3,26 +3,51 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import './App.css';
 
+
 function App() {
+
   // const { data, error, isLoading } = useQuery({key: ['test'], fn:() => {
   //   return axios('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
   //   }
   // })
 
-  const { data, error, isLoading } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: () =>{
-      return axios('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-      //return response
-    }
+  // const priceQuery = useQuery({
+  //   queryKey: ['price'],
+  //   queryFn: () =>{
+  //     return axios('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+  //     //return response
+  //   },
+  //   //refetchInterval: 5000
+  // });
+
+  const customerQuery = useQuery({
+    queryKey: ['customers'], 
+    queryFn: () => {
+    return axios('http://localhost:3000/api/customers')
+    },
+    //refetchInterval: 10000
+    //refetchOnWindowFocus: true,
+    refetchOnWindowFocus: false,
+    //staleTime: 1000 * 1,
+    refetchInterval: 1000,
+    refetchIntervalInBackground: false,
   })
-  return (
-    <div className="App">
-      {error ? <p>Error on display</p> : null}
-      {isLoading ? <p>Loading....</p> : null}
-      {data?.data?.bitcoin?.usd ? data.data.bitcoin.usd : null}
+
+  if(customerQuery?.data?.data?.customers){
+    return <div className="App">
+      {customerQuery.data.data.customers.map((customer) =>{
+        return <p key={customer._id}>{customer.name}</p>
+      })}
     </div>
-  );
+  }
+
+  // return (
+  //   <div className="App">
+  //     {/* {priceQuery?.error ? <p>Error on display</p> : null}
+  //     {priceQuery?.isLoading ? <p>Loading....</p> : null}
+  //     {priceQuery?.data?.data?.bitcoin?.usd ? priceQuery.data.data.bitcoin.usd : null} */}
+  //   </div>
+  // );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { useQuery, useMutation, useQueryClient, QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient(
+//  {
+//   defaultOptions: {
+//     queries: {
+//       refetchOnWindowFocus: false,
+//       //refetchInterval: 1000 * 5,
+//     }
+//   }
+// }
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+          <App />
+        <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </React.StrictMode>
 );
