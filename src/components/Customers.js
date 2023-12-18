1d2eb8d13e4c1f885e391a01c6d9a54451410024
danici <danici@email.com>
2023-12-18 09:08:47 +0100

    caching data 2 and stale data

---
+++ src/components/Customers.js
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const Customers = () => {
+    const customerQuery = useQuery({
+        queryKey: ['customers'], 
+        queryFn: () => {
+        return axios('http://localhost:3000/api/customers')
+        },
+        refetchOnWindowFocus: false
+      } )
+
+      if (customerQuery.isLoading) {
+        return <p>Patience...</p>
+      }
+    
+      if(customerQuery?.data?.data?.customers){
+        return ( <div>
+          {customerQuery.data.data.customers.map((customer) =>{
+            return <p key={customer._id}>{customer.name}</p>
+          })}
+        </div>
+        )
+      }
+      return <div></div>
+}
+
+export default Customers
